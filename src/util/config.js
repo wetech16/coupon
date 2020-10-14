515cd2b0c1b965c1b85e4284e8f9c46c73d5e9ed
Stephen Guo <wetech16@gmail.com>
2020-10-14 10:58:04 -0700

    Update config.js

--- src/util/config.js
+++ src/util/config.js
@@ -1,9 +1,10 @@
 export default {
-  apiKey: "AIzaSyBdJjIw-VEuEQo_xWNP0zSkLav1SzaHJY8",
-  authDomain: "language-ai.firebaseapp.com",
-  databaseURL: "https://language-ai.firebaseio.com",
-  projectId: "language-ai",
-  storageBucket: "language-ai.appspot.com",
-  messagingSenderId: "146060127007",
-  appId: "1:146060127007:web:bc95f0ba0d5994472e9e2a",
+  apiKey: "AIzaSyA7em91ttdAmG2r2Y32TvRWzEroKfmU3OM",
+  authDomain: "coupon-7c1b6.firebaseapp.com",
+  databaseURL: "https://coupon-7c1b6.firebaseio.com",
+  projectId: "coupon-7c1b6",
+  storageBucket: "coupon-7c1b6.appspot.com",
+  messagingSenderId: "78733956764",
+  appId: "1:78733956764:web:b761260b0bcb70efc1bf59",
+  measurementId: "G-PYHEG8F9WT",
 };
