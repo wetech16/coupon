718ec98c60634163590e812dbfb295b14a031e64
Stephen Guo <wetech16@gmail.com>
2020-10-15 18:24:49 -0700

    fixed login staticProfile handle error by adding ...state in SET_USER

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -32,6 +32,7 @@ export default function (state = initialState, action) {
       return initialState;
     case SET_USER:
       return {
+        ...state,
         authenticated: true,
         loading: false,
         ...action.payload,
